48c4e600cca2ac3f44e37dc9dfb6d0ebb6a925dd
Luke Secomb <13529535+lukethacoder@users.noreply.github.com>
2019-11-02 21:36:12 +1100

    updated packages

---
+++ install.ps1
@@ -0,0 +1,11 @@
+choco install nvm -y
+
+choco install yarn jdk8 php python -y
+
+choco install git.install hyper laragon.install sourcetree filezilla putty.install postman -y
+
+choco install spotify -y
+
+choco install 7zip.install slack microsoft-teams.install mailspring office365proplus google-drive-file-stream glasswire 4k-video-downloader handbrake.install lightshot -y
+
+choco install vscode intellijidea-community -y
