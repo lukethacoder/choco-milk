70fac39dcef1131588797acea341e5d9b4083225
Luke Secomb <luke.secomb@live.com>
2021-05-08 12:15:33 +1000

    ✖ add ShareX / add some ref urls

--- install.ps1
+++ install.ps1
@@ -6,6 +6,6 @@ choco install git.install laragon.install filezilla putty.install postman -y
 
 choco install spotify -y
 
-choco install 7zip.install slack microsoft-teams.install googlechrome firefox mailspring office365proplus google-drive-file-stream glasswire 4k-video-downloader handbrake.install firacode -y
+choco install 7zip.install slack microsoft-teams.install googlechrome firefox mailspring office365proplus google-drive-file-stream glasswire 4k-video-downloader handbrake.install firacode sharex -y
 
 choco install vscode intellijidea-community -y
